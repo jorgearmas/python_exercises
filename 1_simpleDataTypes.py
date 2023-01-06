accf7c3f81dca153b38937a317d67f845d29de5d
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-06 13:44:16 -0600

    1_hello_world

--- 1_simpleDataTypes.py
+++ 1_simpleDataTypes.py
@@ -1,6 +1,13 @@
 """
+ENG =>
 Code a program that stores the string Hello World! in a variable and then displays the contents of the variable on the screen.
+
+ESP =>
+Codifique un programa que almacene la cadena ¡Hola Mundo! en una variable y luego muestre el contenido de la variable en la pantalla.
 """
 
+#variable that saves string
 strg = "Hello World!"
+
+#display
 print(strg)
