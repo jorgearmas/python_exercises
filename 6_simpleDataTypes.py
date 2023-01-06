9505c889ef50b75929ce01af3e0c7b43f1ce6347
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-06 13:55:35 -0600

    6_bakery_discount

--- 6_simpleDataTypes.py
+++ 6_simpleDataTypes.py
@@ -1,5 +1,9 @@
 """
+ENG =>
 A bakery sells loaves of bread at €3.49 each. The non-daily bread is discounted by 60%. Write a program that starts by reading the number of loaves sold that are not of the day. Then the program should display the usual price of a loaf of bread, the discount for non-fresh and the total final cost.
+
+ESP =>
+Una panadería vende barras de pan a 3,49 euros cada una. El pan que no es del día tiene un descuento del 60%. Escribe un programa que empiece leyendo el número de barras de pan vendidas que no son del día. A continuación, el programa debe mostrar el precio habitual de una barra de pan, el descuento por no ser del día y el coste final total.
 """
 print("")
 #number of non-fresh loafs requested
