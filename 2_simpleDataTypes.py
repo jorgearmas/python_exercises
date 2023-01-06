e05e95c7c73566571c14079f8bd56d68945fc1f2
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-06 13:46:27 -0600

    2_arthmetic operation

--- 2_simpleDataTypes.py
+++ 2_simpleDataTypes.py
@@ -1,6 +1,13 @@
 """
+ENG =>
 Code a program that displays on the screen the result of the following arithmetic operation (3+2/2*5)**2
+
+ESP =>
+Codifica un programa que muestre en pantalla el resultado de la siguiente operación aritmética (3+2/2*5)**2
 """
 
+#operation
 operation = ((3+2)/(2*5)**2)
+
+#display
 print(f"The result of the operation is: {operation}")
