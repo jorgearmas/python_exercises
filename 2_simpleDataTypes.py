f19656c4fbefa1e82d40b3819b99bbe2753fa553
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-05 20:06:55 -0600

    commit 2, completed

---
+++ 2_simpleDataTypes.py
@@ -0,0 +1,6 @@
+"""
+Code a program that displays on the screen the result of the following arithmetic operation (3+2/2*5)**2
+"""
+
+operation = ((3+2)/(2*5)**2)
+print(f"The result of the operation is: {operation}")
