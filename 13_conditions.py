1a4db4eee15f7361294bb06b9e62066f4c7c4de8
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-12 22:18:46 -0600

    age verification

---
+++ 13_conditions.py
@@ -0,0 +1,20 @@
+"""
+ENG =>
+Code a program that asks the user his age and displays on the screen whether he is of age or not.
+
+Age condition > 21
+
+ESP =>
+Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+Condicion de edad > 21
+"""
+#input
+print("")
+age = int(input("What is your age? "))
+print("")
+#condition / output
+if age >= 21:
+    print("You are of age")
+else:
+    print("You are underage")
