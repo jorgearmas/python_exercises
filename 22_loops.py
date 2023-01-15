1d7a4e0c862847d0b6cc02efd917becd298b6acc
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-14 23:17:20 -0600

    22_multiplication

---
+++ 22_loops.py
@@ -0,0 +1,68 @@
+"""
+ENG =>
+Write a program that displays the multiplication table from 1 to 10.
+
+ESP =>
+Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
+"""
+#global variables
+iterator = 1
+
+#loops / output
+while iterator <= 10:
+    print(f"1 * {iterator} = {1*iterator}")
+    iterator += 1
+
+print("-"*10)
+iterator = 1
+while iterator <= 10:
+    print(f"2 * {iterator} = {2*iterator}")
+    iterator += 1
+
+print("-"*10)
+iterator = 1
+while iterator <= 10:
+    print(f"3 * {iterator} = {3*iterator}")
+    iterator += 1
+
+print("-"*10)
+iterator = 1
+while iterator <= 10:
+    print(f"4 * {iterator} = {4*iterator}")
+    iterator += 1
+
+print("-"*10)
+iterator = 1
+while iterator <= 10:
+    print(f"5 * {iterator} = {5*iterator}")
+    iterator += 1
+
+print("-"*10)
+iterator = 1
+while iterator <= 10:
+    print(f"6 * {iterator} = {6*iterator}")
+    iterator += 1
+
+print("-"*10)
+iterator = 1
+while iterator <= 10:
+    print(f"7 * {iterator} = {7*iterator}")
+    iterator += 1
+
+print("-"*10)
+iterator = 1
+while iterator <= 10:
+    print(f"8 * {iterator} = {8*iterator}")
+    iterator += 1
+
+print("-"*10)
+iterator = 1
+while iterator <= 10:
+    print(f"9 * {iterator} = {9*iterator}")
+    iterator += 1
+
+print("-"*10)
+iterator = 1
+while iterator <= 10:
+    print(f"10 * {iterator} = {10*iterator}")
+    iterator += 1
