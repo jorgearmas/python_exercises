3fee52348e4bc07e13857ebf04fc1219d1aa3e24
Jorge Armas <jorgearmasc0496@gmail.com>
2023-01-31 16:46:22 -0600

    to be completed

---
+++ 34_dictionaries.py
@@ -0,0 +1,7 @@
+"""
+ENG =>
+Write a program that asks the user for his name, age, address and telephone number and stores it in a dictionary. It should then display the message <name> is <age> years old, lives at <address> and his phone number is <phone>.
+
+ESP =>
+Escribir un programa que pregunte al usuario su nombre, edad, dirección y teléfono y lo guarde en un diccionario. Después debe mostrar por pantalla el mensaje <nombre> tiene <edad> años, vive en <dirección> y su número de teléfono es <teléfono>.
+"""
